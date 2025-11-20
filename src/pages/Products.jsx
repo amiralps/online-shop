08a86ce4f9d32488997e6b26917274505aef7efa
amiralps <farzanetoosi001@gmail.com>
2025-11-20 23:29:33 +0330

    shop

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,25 +1,37 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
-import styles from "../styles/Products.module.css"
+import styles from "../styles/Products.module.css";
 
 function Products() {
   const [products, setProducts] = useState([]);
   useEffect(() => {
     axios.get("products/data.json").then((res) => setProducts(res.data));
   }, []);
-  if (!products.length) return <h1>Loading ...</h1>
+  console.log(products);
+  if (!products.length) return <h1>Loading ...</h1>;
   return (
     <>
       <ul>
         {products.map((i) => (
-            <li key={i.id}>
-              <h1>{i.title}</h1>
-              {i.images.map((image, index) => (
-                <img className={styles.image} key={index} src={image} alt={i.title} />
+          <li key={i.id}>
+            <h1>{i.title}</h1>
+            {i.images.map((image, index) => (
+              <img
+                className={styles.image}
+                key={index}
+                src={image}
+                alt={i.title}
+              />
+            ))}
+            <p>{i.description}</p>
+            <ul className={styles.colors}>
+              {i.colors.map((items, index) => (
+                <li style={{backgroundColor: items.code}} key={index}></li>
               ))}
-              <p>{i.description}</p>
-            </li>
-          ))}
+              : رنگ ها
+            </ul>
+          </li>
+        ))}
       </ul>
     </>
   );
