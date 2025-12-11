ce9ad6c05986e967dfe3103b8e1aac5da0c18adb
amiralps <farzanetoosi001@gmail.com>
2025-12-11 22:27:29 +0330

    shop

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,14 @@
+import { priceFormat } from "../helper/helper";
 import styles from "../styles/Card.module.css";
 import {Link} from "react-router-dom";
-function Card({data: {id, title, images, description}}) {
+function Card({data: {id, title, images, colors}}) {
   return (
     <>
       <Link className={styles.link} to={`/products/${id}`}>
         <div className={styles.card}>
           <img className={styles.bgimage} src={images[0]} alt={title}></img>
           <h1>{title}</h1>
-          <p>{description}</p>
+          <p>{priceFormat(colors[0].price)}</p>
         </div>
       </Link>
     </>
