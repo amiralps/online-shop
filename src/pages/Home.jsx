f76ddbaee12530d0dd273898d432f12aa00c8b55
amiralps <farzanetoosi001@gmail.com>
2025-12-14 14:22:48 +0330

    shop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,16 +8,27 @@ function Home() {
   }, []);
   return (
     <div className={styles.container}>
-      <MySunglasses
-        data={{
-          duration: "6s",
-          fill: "forwards",
-          count: "infinite",
-          ease: "linear",
-          strokeWidth: "6"
-        }}
-      />
-      <h1>عینک اُپتیک</h1>
+      <div className={styles.svg}>
+        <MySunglasses
+          data={{
+            duration: "6s",
+            fill: "forwards",
+            count: "infinite",
+            ease: "linear",
+            strokeWidth: "6",
+          }}
+        />
+      </div>
+      <div className={styles.text}>
+        <h1>عینک اُپتیک</h1>
+        <div className={styles.items}>
+          <div>
+            <span>زیبا</span>
+            <span>مقاوم</span>
+            <span>به صرفه</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
