0008ab23251d4f850fc750e52e7300e88b43a037
amiralps <farzanetoosi001@gmail.com>
2025-12-17 00:55:41 +0330

    shop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import {Link, NavLink} from "react-router-dom";
 import styles from "../styles/Header.module.css";
+import TMStyle from "../styles/ToggleMenu.module.css";
 import {LiaShoppingCartSolid} from "react-icons/lia";
 import {useSelector} from "react-redux";
 import {RiMoonClearLine} from "react-icons/ri";
 import {BsFillSunFill} from "react-icons/bs";
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function Header() {
+  const toggleMenu = useRef(null);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 900);
   const [theme, setTheme] = useState(localStorage.getItem("theme"));
   function themeHandler() {
     if (theme === "Dark") {
@@ -19,13 +22,21 @@ function Header() {
       setTheme("Dark");
     }
   }
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setIsMobile(window.innerWidth <= 900);
+    });
+    return window.removeEventListener("resize", () => {
+      setIsMobile(window.innerWidth <= 900);
+    });
+  }, []);
   const {selectedItems, itemsCounter, totalCount} = useSelector(
     (state) => state.cart
   );
   return (
     <>
       <div className={styles.header}>
-        <div className={styles.rightdiv}>
+        <div className={styles.logo}>
           <h3>
             <Link to={"/"}>
               <img src="/policy-sunglasses.svg" alt="logo" />
@@ -33,43 +44,73 @@ function Header() {
             </Link>
           </h3>
         </div>
-        <div className={styles.centerdiv}>
-          <ul>
-            <li>
-              <NavLink to="/">خانه</NavLink>
-            </li>
-            <li>
-              <NavLink to="products">محصولات</NavLink>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.leftdiv}>
-          <ul>
-            <li className="cart">
-              <NavLink to="/shopping-cart">
-                <LiaShoppingCartSolid />
-                {itemsCounter ? <div>{itemsCounter}</div> : ""}
-              </NavLink>
-            </li>
-            <li>
-              <button
-                className={styles.themeBtn}
-                onClick={() => {
-                  themeHandler()
-                  // if (localStorage.getItem("theme") === "Dark") {
-                  //   localStorage.removeItem("theme");
-                  //   document.querySelector("html").classList.remove("Dark");
-                  // } else {
-                  //   localStorage.setItem("theme", "Dark");
-                  //   document.querySelector("html").classList.add("Dark");
-                  // }
-                }}>
-                <RiMoonClearLine className={styles.moon} />
-                <BsFillSunFill className={styles.sun} />
-              </button>
-            </li>
-          </ul>
-        </div>
+        {!isMobile ? (
+          <>
+            <div className={styles.centerdiv}>
+              <ul>
+                <li>
+                  <NavLink to="/">خانه</NavLink>
+                </li>
+                <li>
+                  <NavLink to="products">محصولات</NavLink>
+                </li>
+              </ul>
+            </div>
+            <div className={styles.leftdiv}>
+              <ul>
+                <li className="cart">
+                  <NavLink to="/shopping-cart">
+                    <LiaShoppingCartSolid />
+                    {itemsCounter ? <div>{itemsCounter}</div> : ""}
+                  </NavLink>
+                </li>
+                <li>
+                  <button
+                    className={styles.themeBtn}
+                    onClick={() => {
+                      themeHandler();
+                      // if (localStorage.getItem("theme") === "Dark") {
+                      //   localStorage.removeItem("theme");
+                      //   document.querySelector("html").classList.remove("Dark");
+                      // } else {
+                      //   localStorage.setItem("theme", "Dark");
+                      //   document.querySelector("html").classList.add("Dark");
+                      // }
+                    }}>
+                    <RiMoonClearLine className={styles.moon} />
+                    <BsFillSunFill className={styles.sun} />
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </>
+        ) : (
+          <>
+            <div
+              className={TMStyle.menu}
+              ref={toggleMenu}
+              onClick={() => {
+                toggleMenu.current.classList.toggle(TMStyle.open);
+              }}>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div
+              className={TMStyle.leftContainer}
+              onClick={() => {
+                toggleMenu.current.classList.remove(TMStyle.open);
+              }}></div>
+            <div className={TMStyle.list}>
+              <ul>
+                <li><NavLink to={"/"} >خانه</NavLink></li>
+                <li><NavLink to={"/products"} >محصولات</NavLink></li>
+                <li><NavLink to={"shopping-cart"} >سبد خرید</NavLink></li>
+                {/* <li><NavLink to={""} ></NavLink></li> */}
+              </ul>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
