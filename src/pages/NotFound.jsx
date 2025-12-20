349cae5e2dc1a89523c51e78520bded6203fa361
amiralps <farzanetoosi001@gmail.com>
2025-12-20 20:30:14 +0330

    shop

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,15 @@
 import { useEffect } from "react";
 import { changeTitle } from "../helper/helper";
+import styles from "../styles/NotFound.module.css"
 
 function NotFound() {
   useEffect(() => {
     changeTitle("یافت نشد");
   }, []);
   return (
-	<div>
+	<div className={styles.notFound}>
 		<h1>404</h1>
-		<h3>Page NotFound</h3>
+		<h3>صفحه یافت نشد!</h3>
 	</div>
   )
 }
